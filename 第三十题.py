f8c6f3a124fbd51f60fdcacb2a58cf00248bbd61
baolongsun <212796848@qq.com>
2024-04-04 12:36:28 +0800

    层序遍历树

---
+++ 第三十题.py
@@ -0,0 +1,40 @@
+# 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+# 给定二叉树: [3,9,20,null,null,15,7]
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+#返回 [3,9,20,15,7]
+
+class TreeNode(object):
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def levelOrder(root):
+    deque = [root]
+    result = []
+    if not root:#判断空情况
+        return []
+    while deque:
+        poped = deque.pop(0)
+        if poped.left:
+            deque.append(poped.left)
+        if poped.right:
+            deque.append(poped.right)
+        result.append(poped.val)
+    return result
+
+if __name__ == "__main__":
+    T1 = TreeNode(3)
+    T2 = TreeNode(9)
+    T3 = TreeNode(20)
+    T4 = TreeNode(15)
+    T5 = TreeNode(7)
+    T1.left = T2
+    T1.right = T3
+    T3.left = T4
+    T3.right = T5
+    print(levelOrder(T1))
